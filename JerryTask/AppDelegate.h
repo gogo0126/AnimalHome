2c7d0e0984413dff52b5fabd616a290fa6da5b1f
jerryliao <jerryliao@uitox.com>
2017-02-18 11:19:23 +0800

    Initial Commit

---
+++ JerryTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JerryTask
+//
+//  Created by jerryliao on 2017/2/18.
+//  Copyright © 2017年 jerryliao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JerryTask/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JerryTask
+//
+//  Created by jerryliao on 2017/2/18.
+//  Copyright © 2017年 jerryliao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
